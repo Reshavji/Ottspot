8b35380f97170087759863c84b786f10a9d8d960
Reshavji <reshavkumar9837@gmail.com>
2023-12-07 13:04:00 +0530

    Update

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,6 +5,18 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="app-logo"><img id="logo" src={logo} alt="logo" /></div>
+        <div className='navbar'>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Platforms</li>
+                    <li>Services</li>
+                    <li>Support & FAQ</li>
+                    <li>Contact Us</li>
+                </ul>
+            </nav>
+            <button className='purchase-button'>Purchase Now</button>
+        </div>
     </div>
   )
 }

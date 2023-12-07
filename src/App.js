cb4dda0010f8fe2addae91e3ed2f240dad4e098a
Reshavji <reshavkumar9837@gmail.com>
2023-12-07 14:35:20 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './Header/Header';
+import Home from './Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-       
+      <Home />
     </div>
   );
 }

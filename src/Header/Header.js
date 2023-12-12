1af8b6e336a29a241e6384ff11a3b565ba905fa1
Reshavji <reshavkumar9837@gmail.com>
2023-12-12 15:54:23 +0530

    Update

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import logo from "../Images/Ottspot.png"
 import './Header.css';
+
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle the menu state
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  // Add scroll event listener to change header color on scroll
   window.addEventListener('scroll', function() {
     const header = document.querySelector('.header');
     const scrollPosition = window.scrollY;
@@ -13,22 +22,29 @@ const Header = () => {
       header.classList.remove('black');
     }
   });
-  
+
   return (
     <div className='header'>
-        <div className="app-logo"><Link to="/"><img id="logo" src={logo} alt="logo" /></Link></div>
-        <div className='navbar'>
-            <nav>
-                <ul>
-                    <Link to ="/"><li>Home</li></Link>
-                    <Link to="/platforms"><li>Platforms</li></Link>
-                    <li>Services</li>
-                    <li>Support & FAQ</li>
-                    <li>Contact Us</li>
-                </ul>
-            </nav>
-            <button className='purchase-button'>Purchase Now</button>
+      <div className="app-logo">
+        <Link to="/">
+          <img id="logo" src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div className='navbar'>
+        <nav className={isMenuOpen ? 'active' : ''}>
+          <ul>
+            <Link to="/"><li>Home</li></Link>
+            <Link to="/platforms"><li>Platforms</li></Link>
+            <li>Services</li>
+            <li>Support & FAQ</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+        <button className='purchase-button'>Purchase Now</button>
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <div className={isMenuOpen ? 'hamburger open' : 'hamburger'}></div>
         </div>
+      </div>
     </div>
   )
 }
